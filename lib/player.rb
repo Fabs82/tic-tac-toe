b757e6f30c2e05e2b4a76a5b600c17f30f8070ba
Fabs82 <148846635+Fabs82@users.noreply.github.com>
2025-07-23 09:39:43 +0200

    Edit Player class adding name and player_two

--- lib/player.rb
+++ lib/player.rb
@@ -5,7 +5,7 @@ class Player
   def initialize(name, symbol)
     @name = name
     @symbol = symbol
-    puts "Object initialized - Name: #{@name} | Symbol: #{@symbol}"
+    puts "Player initialized - Name: #{@name} | Symbol: #{@symbol}"
   end
 
   def make_choice
@@ -15,10 +15,12 @@ class Player
       puts 'Position not valid. Please make your choice (a number  between 1 and 9)'
       position = gets.chomp.to_i
     end
-    puts "You chose #{position}"
+    puts "#{@name} chose #{position}"
     position
   end
 end
 
 player_one = Player.new('Fabio', 'X')
+player_two = Player.new('Biagio', 'O')
 player_one.make_choice
+player_two.make_choice
