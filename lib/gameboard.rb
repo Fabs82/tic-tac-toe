3b09c8f7028ab7d6eb198daf87a58426c88bda98
Fabs82 <148846635+Fabs82@users.noreply.github.com>
2025-07-24 12:17:19 +0200

    Add check_poistion? method
    A method that checks if the position entered by the player is valid or not

--- lib/gameboard.rb
+++ lib/gameboard.rb
@@ -1,25 +1,30 @@
 # Classe che crea e gestisce la gameboard. Controlla se la cell é disponibile e posiziona la scelta del Player
 class Gameboard
-  def initialize(symbol, position)
-    @symbol = symbol
-    @position = position
-    board = []
+  def initialize
+    @board = []
     n = 1
     3.times do |nested_array|
-      board[nested_array] = []
+      @board[nested_array] = []
       3.times do |cell|
-        board[nested_array][cell] = n
+        @board[nested_array][cell] = n
         n += 1
       end
-      p board[nested_array]
+      p @board[nested_array]
     end
   end
 
-  def check_position
+  def check_position?(position)
+    @board.each do |nested_array|
+      nested_array.each do |cell|
+        return true if cell == position
+      end
+    end
+    false
   end
 
   def update_position
   end
 end
 
-gameboard = Gameboard.new('X', 2)
+gameboard = Gameboard.new
+p gameboard.check_position?(4)
